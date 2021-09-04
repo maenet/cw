2739d057f9407c2e782c0a759e270ef989eff3ee
Takahiro Maeda <t.maeda@mynet.co.jp>
2021-09-05 02:41:17 +0900

    implement methods for endpoint /me, /my and /contacts

--- client.go
+++ client.go
@@ -1,11 +1,15 @@
 package chatwork
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"path"
 )
 
 type Client struct {
@@ -36,4 +40,105 @@ func NewClient(rawURL string, token string, logger *log.Logger) (*Client, error)
 		Token:      token,
 		Logger:     logger,
 	}, nil
+}
+
+func (c *Client) newRequest(ctx context.Context, method string, spath string, body io.Reader) (*http.Request, error) {
+	u := *c.BaseURL
+	u.Path = path.Join(c.BaseURL.Path, spath)
+
+	req, err := http.NewRequestWithContext(ctx, method, u.String(), body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("X-ChatWorkToken", c.Token)
+
+	return req, nil
+}
+
+func decodeBody(resp *http.Response, out interface{}) error {
+	defer resp.Body.Close()
+	decoder := json.NewDecoder(resp.Body)
+	return decoder.Decode(out)
+}
+
+func (c *Client) GetAccount(ctx context.Context) (*GetAccountResponse, error) {
+	spath := "/me"
+	req, err := c.newRequest(ctx, http.MethodGet, spath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var body GetAccountResponse
+	if err := decodeBody(res, &body); err != nil {
+		return nil, err
+	}
+
+	return &body, nil
+}
+
+func (c *Client) GetStatus(ctx context.Context) (*GetStatusResponse, error) {
+	spath := "/my/status"
+	req, err := c.newRequest(ctx, http.MethodGet, spath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var body GetStatusResponse
+	if err := decodeBody(res, &body); err != nil {
+		return nil, err
+	}
+
+	return &body, nil
+}
+
+func (c *Client) ListTasks(ctx context.Context) (*ListTasksResponse, error) {
+	spath := "/my/tasks"
+	req, err := c.newRequest(ctx, http.MethodGet, spath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var body ListTasksResponse
+	if err := decodeBody(res, &body); err != nil {
+		return nil, err
+	}
+
+	return &body, nil
+}
+
+func (c *Client) ListContacts(ctx context.Context) (*ListContactsResponse, error) {
+	spath := "/contacts"
+	req, err := c.newRequest(ctx, http.MethodGet, spath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var body ListContactsResponse
+	if err := decodeBody(res, &body); err != nil {
+		return nil, err
+	}
+
+	return &body, nil
 }
